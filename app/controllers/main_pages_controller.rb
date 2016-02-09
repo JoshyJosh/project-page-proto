3836446800263bf4dd8ab13c0327750af84f1e51
JoshyJosh <herukula@gmail.com>
2016-02-09 23:44:14 +0100

    Added Date and Title sortation, still need categories

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -1,18 +1,31 @@
 class MainPagesController < ApplicationController
+	helper_method :sort_column, :sort_direction
 	
 	@@proj_categories = Category.all
 	@@projects = Project.all
 	
   def home
-		@some_stuff = @@proj_categories
+		@some_stuff = sort_column
   end
   
   def about
   end
 
   def proj_list
-		@projects = @@projects
+		@projects = Project.order(sort_column + " " + sort_direction)
 		@proj_categories = @@proj_categories.select(:cat_title).distinct
-		@test_string = "<p>This is a test it should be a paragraph</p>"
+		@test_down_carret = "▾"
+		@test_up_carret = "▴"
   end
+  
+  private
+  
+  #TODO for sorting the app table
+  def sort_column
+		Project.column_names.include?(params[:sort]) ? params[:sort] : "publish_date"
+	end
+	
+	def sort_direction
+		%w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
+	end
 end
--- app/helpers/main_pages_helper.rb
+++ app/helpers/main_pages_helper.rb
@@ -1,2 +1,12 @@
 module MainPagesHelper
+	def sortable(column, title = nil)
+		direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
+		
+		# check if sort parameter is being used
+		if params[:sort] == column
+			title += direction == "asc" ? " ▾" : " ▴"
+		end
+		
+		link_to title, :sort => column, :direction => direction
+	end
 end
